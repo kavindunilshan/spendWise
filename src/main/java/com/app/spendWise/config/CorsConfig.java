177aea4cfa6c4a5e36e3e7106d101052552bfc15
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-25 12:02:05 +0530

    Cors config allow to all domains

--- src/main/java/com/app/spendWise/config/CorsConfig.java
+++ src/main/java/com/app/spendWise/config/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Replace with your Vite development server URL
+                        .allowedOrigins("*") // Allow all origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
