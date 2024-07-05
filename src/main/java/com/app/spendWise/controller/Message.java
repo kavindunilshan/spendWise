902137971cda1c8d7f360ca90b41eee8571fc1e1
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-07-05 18:05:40 +0530

    Testing security

---
+++ src/main/java/com/app/spendWise/controller/Message.java
@@ -0,0 +1,13 @@
+package com.app.spendWise.controller;
+
+public class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
