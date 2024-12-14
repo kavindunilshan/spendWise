7af4ac82ed27a03dd95a52830e5c3dfe0bd46b58
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-12-15 01:29:41 +0530

    Milestone entity created.

---
+++ src/main/java/com/app/spendWise/service/MilestoneService.java
@@ -0,0 +1,33 @@
+package com.app.spendWise.service;
+
+import com.app.spendWise.entity.Milestone;
+import com.app.spendWise.repository.MilestoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MilestoneService {
+    @Autowired
+    private MilestoneRepository milestoneRepository;
+
+    public List<Milestone> getMilestones(String userId) {
+        return milestoneRepository.findByUser_id(userId);
+    }
+
+    public Milestone createMilestone(Milestone milestone) {
+        return milestoneRepository.save(milestone);
+    }
+
+    public void deleteMilestone(int id) {
+        milestoneRepository.deleteById(id);
+    }
+
+    public Milestone updateMilestone(int id, Milestone milestone) {
+        milestone.setMilestoneId(id);
+        milestoneRepository.save(milestone);
+        return milestone;
+    }
+
+}
